991e74349a93ed08e462044e555c693de5adcd19
Aaron Christy <aaron@phad.com>
2024-09-24 18:37:10 -0400

    Nest connection specs properly
    We are going to want to add additional tests, so let's properly organize
    these tests.

--- spec/flockbot/connection_spec.rb
+++ spec/flockbot/connection_spec.rb
@@ -1,14 +1,18 @@
 require "spec_helper"
 
-RSpec.describe "Flockbot::Connection", :vcr do
-  it "raises error with invalid flocknote credentials" do
-    service = Flockbot::Connection.new(subdomain: "subdomain", email: "me@example.com", password: "incorrect")
-    expect { service.connect! }.to raise_error { Flockbot::TransactionError }
+RSpec.describe Flockbot::Connection, :vcr do
+  context "with invalid flocknote credentials" do
+    it "raises an error" do
+      service = Flockbot::Connection.new(subdomain: "subdomain", email: "me@example.com", password: "incorrect")
+      expect { service.connect! }.to raise_error { Flockbot::TransactionError }
+    end
   end
 
-  it "connects successfully with valid flocknote credentials" do
-    service = Flockbot::Connection.new(subdomain: "subdomain", email: "me@example.com", password: "mypassword")
-    service.connect!
-    expect(service.connected).to eq(true)
+  context "with valid flocknote credentials" do
+    it "connects successfully" do
+      service = Flockbot::Connection.new(subdomain: "subdomain", email: "me@example.com", password: "mypassword")
+      service.connect!
+      expect(service.connected).to eq(true)
+    end
   end
 end
