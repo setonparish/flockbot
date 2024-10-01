77631405db6825a4ef131f3312796e7849d9d0df
Aaron Christy <aaron@phad.com>
2024-10-01 13:41:54 -0400

    Simplify Flockbot::Models::Group
    This is a refactor on this model removing some cruft and making things
    more easily understandable.
    
    Big changes:
    
    * the model now takes a session object that is already logged in
    * pass in explicit arguments

--- spec/flockbot/models/group_spec.rb
+++ spec/flockbot/models/group_spec.rb
@@ -1,91 +1,119 @@
 require "spec_helper"
 
-RSpec.describe "Flockbot::Models::Group", :vcr do
-  let(:service) { Flockbot::Models::Group.new(attrs, connection) }
-  let(:connection) { double(:connection, get: response, post: response) }
-  let(:response) { {} }
+RSpec.describe Flockbot::Models::Group, :vcr do
+  let(:flocknote_group_id) do
+    # an actual existing group id in flocknote
+    "931288"
+  end
+
+  let(:session) do
+    Flockbot::Session.new(
+      subdomain: ENV["FLOCKNOTE_SUBDOMAIN"],
+      email: ENV["FLOCKNOTE_USER_EMAIL"]
+    )
+  end
 
   describe "#initialize" do
-    let(:attrs) do
-      {
-        id: "1",
+    let(:group) do
+      Flockbot::Models::Group.new(
+        id: flocknote_group_id,
         name: "Everyone",
         short_name: "everyone",
-      }
+        session:
+      )
     end
 
-    it "sets correct attributes" do
-      aggregate_failures do
-        expect(service.id).to eq("1")
-        expect(service.name).to eq("Everyone")
-        expect(service.short_name).to eq("everyone")
-      end
+    it "sets correct attributes", aggregate_failures: true do
+      expect(group.id).to eq(flocknote_group_id)
+      expect(group.name).to eq("Everyone")
+      expect(group.short_name).to eq("everyone")
     end
   end
 
   describe "#everyone?" do
+    let(:group) do
+      Flockbot::Models::Group.new(
+        id: flocknote_group_id,
+        name: "My Group",
+        short_name: short_name,
+        session:
+      )
+    end
+
     context "with a short name of 'everyone'" do
-      let(:attrs) do
-        { short_name: "everyone" }
-      end
+      let(:short_name) { "everyone" }
 
       it "is true" do
-        expect(service.everyone?).to eq(true)
+        expect(group.everyone?).to eq(true)
       end
     end
 
     context "without a short name of 'everyone'" do
-      let(:attrs) do
-        { short_name: "not everyone" }
-      end
+      let(:short_name) { "not everyone" }
 
       it "is false" do
-        expect(service.everyone?).to eq(false)
+        expect(group.everyone?).to eq(false)
       end
     end
   end
 
   describe "#subscriber_count" do
-    let(:attrs) do
-      { "id" => 999 }
+    before do
+      session.login!(password: ENV["FLOCKNOTE_USER_PASSWORD"])
     end
 
-    let(:response) do
-      { "subscriberCount" => "333" }
+    let(:group) do
+      Flockbot::Models::Group.new(
+        id: flocknote_group_id,
+        name: "Test Group",
+        short_name: "test group",
+        session:
+      )
     end
 
-    it "extracts the subscriber count as an integer" do
-      expect(service.subscriber_count).to eq(333)
+    it "returns subscriber count" do
+      expect(group.subscriber_count).to eq(2)
     end
   end
 
   describe "#add_user" do
-    let(:attrs) do
-      { id: "999" }
+    before do
+      session.login!(password: ENV["FLOCKNOTE_USER_PASSWORD"])
     end
 
-    let(:response) do
-      { "success" => true }
-    end
-
-    it "adds a new user to a group" do
-      params = {
-        first_name: "First",
-        last_name: "Last",
-        email: "me@example.com",
-        mobile_phone: "555-555-5555"
-      }
-
-      expect(connection).to receive(:post).with(
-        "/group/999/addToGroupByAdmin",
-        {
-          fname: "First",
-          lname: "Last",
-          email: "me@example.com",
-          mobile_phone: "555-555-5555"
-        }
+    let(:group) do
+      Flockbot::Models::Group.new(
+        id: flocknote_group_id,
+        name: "Test Group",
+        short_name: "test group",
+        session:
       )
-      expect(service.add_user(**params)).to eq(true)
+    end
+
+    context "with valid user options" do
+      it "adds a new user to a group" do
+        expect(
+          group.add_user(
+            first_name: "Another",
+            last_name: "Test User",
+            email: "test@example.com",
+            mobile_phone: nil
+          )
+        ).to eq(true)
+      end
+    end
+
+    context "with invalid user options" do
+      it "raises a transaction error" do
+        expect {
+          group.add_user(
+            first_name: "Another",
+            last_name: "Test User",
+            email: "not-a-vaild-email",
+            mobile_phone: nil
+          )
+        }.to raise_error(Flockbot::TransactionError, /something wrong with that email address/)
+      end
     end
   end
 end
